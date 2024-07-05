db02e1d333c6fd5d65aa8ee580697dc10e16a6be
satish-nirmal <satishnirmal16@gmail.com>
2024-07-05 10:18:51 +0530

    find the everse number

---
+++ ReverseNumber.java
@@ -0,0 +1,31 @@
+package com.number.problems;
+
+/**
+ * Question :
+ * 1. Find to Reverse the given number 
+ */
+
+/**
+ * @author Satish
+ */
+
+public class ReverseNumber {
+	
+	public static void main(String[] args) {
+
+		int num = 123456789;
+
+		int rem, rev = 0;
+
+		while (num != 0) {
+
+			rem = num % 10;
+			rev = rev * 10 + rem;
+			num = num / 10;
+		}
+
+		System.out.println(rev);
+
+	}
+
+}
