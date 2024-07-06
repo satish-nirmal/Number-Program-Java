2dfc89703faa5d2b78d974cd39eaa40805508819
satish-nirmal <satishnirmal16@gmail.com>
2024-07-06 13:13:51 +0530

    identified given number is palindrom

---
+++ PalindromNumber.java
@@ -0,0 +1,27 @@
+package com.number.problems;
+
+public class PalindromNumber {
+
+	public static void main(String[] args) {
+
+		int num = 121;
+
+		int rem, rev = 0;
+		int temp = num;
+
+		while (temp != 0) {
+
+			rem = temp % 10;
+			rev = rev * 10 + rem;
+			temp = temp / 10;
+		}
+
+		if (num == rev) {
+			System.out.println("This is Palindrom Number");
+		} else {
+			System.out.println("Not Palindrom Number");
+
+		}
+	}
+
+}
