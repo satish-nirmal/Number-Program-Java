24570509d2966b6cd398edb0f3450529c14b21c8
satish-nirmal <satishnirmal16@gmail.com>
2024-07-10 20:04:17 +0530

    updated

--- SwapNumberUsingBetwise.java
+++ SwapNumberUsingBetwise.java
@@ -1,11 +1,19 @@
 package com.number.problems;
 
+/**
+ * Question :
+ * 1. Swap number using bitwise operator. 
+ */
+
+/**
+ * @author Satish
+ */
+
 public class SwapNumberUsingBetwise {
 
 	public static void main(String[] args) {
 		int a = 8;
 		int b = 10;
-		System.out.println("Before swaping : a = " + a + " b = " + b);
 		a = a ^ b ^ (b = a);
 		System.out.println("After swaping : a = " + a + " b = " + b);
 	}
