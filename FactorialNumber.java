bb2642a81d13bb2628c422534bc4a48cbeaf6abc
satish-nirmal <satishnirmal16@gmail.com>
2024-07-11 20:52:02 +0530

    find the given numbers factorial

---
+++ FactorialNumber.java
@@ -0,0 +1,20 @@
+package com.number.problems;
+
+public class FactorialNumber {
+
+		public static void main(String[] args) {
+			
+			int num = 10 ;
+			
+			int fact = 1 ;
+			
+			
+			for(int i= 1; i<= num ; i++) {
+				
+				fact = fact * i;
+			}
+			
+			System.out.println(fact);
+			
+		}
+}
