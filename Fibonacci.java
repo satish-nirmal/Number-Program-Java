ad5158baf710344a46bf503a4ef980bb593f646d
satish-nirmal <satishnirmal16@gmail.com>
2024-07-13 15:08:03 +0530

    Fibonacci series in java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package com.number.problems;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+
+		int a = 0 ; int b = 1;
+		int c ;
+		
+		
+		for(int i = 1 ; i <= 10 ; i++) {
+			
+			c = a+ b ;
+			
+			a= b ;
+			b = c ;
+			
+			
+			System.out.println(c);
+		}
+
+	}
+
+}
