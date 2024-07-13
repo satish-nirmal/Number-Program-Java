338eef6b5b8875c9bbc48ba9dbb3731633bda567
satish-nirmal <satishnirmal16@gmail.com>
2024-07-13 15:09:19 +0530

    updated

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,21 +1,29 @@
 package com.number.problems;
 
+/**
+ * Question :
+ * 1. Find to Fibonacci series the given number 
+ */
+
+/**
+ * @author Satish
+ */
+
 public class Fibonacci {
-	
+
 	public static void main(String[] args) {
 
-		int a = 0 ; int b = 1;
-		int c ;
-		
-		
-		for(int i = 1 ; i <= 10 ; i++) {
-			
-			c = a+ b ;
-			
-			a= b ;
-			b = c ;
-			
-			
+		int a = 0;
+		int b = 1;
+		int c;
+
+		for (int i = 1; i <= 10; i++) {
+
+			c = a + b;
+
+			a = b;
+			b = c;
+
 			System.out.println(c);
 		}
 
